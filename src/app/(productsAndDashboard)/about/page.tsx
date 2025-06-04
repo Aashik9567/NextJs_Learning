74d973f296a109b9909ff400efc66dedab791a83
AashiQ <aashikmahato9567@gmail.com>
2025-06-04 23:43:01 +0545

    Update metadata for About page and adjust layout title for consistency

--- src/app/(productsAndDashboard)/about/page.tsx
+++ src/app/(productsAndDashboard)/about/page.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
-
+export const metadata = {
+  title: 'About Page',
+  description: 'This is the about page of the Next.js application.'
+}
 const page = () => {
   return (
     <div className='bg-gray-900 text-white p-4'>
